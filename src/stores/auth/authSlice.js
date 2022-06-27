9abc78db32cc0bf45e9fc738360d2f1fb8d0976a
kimhuy011199 <kimhuy011199@gmail.com>
2022-06-27 23:06:59 +0700

    Test auth store

--- src/stores/auth/authSlice.js
+++ src/stores/auth/authSlice.js
@@ -3,17 +3,6 @@ import authStorageService from '../../core/authStorage.service';
 import { authType } from './authType';
 import authService from './authService';
 
-export interface LoginUserInterface {
-  email: string;
-  password: string;
-}
-
-export interface RegisterUserInterface {
-  displayName: string;
-  email: string;
-  password: string;
-}
-
 // Get user from localStorage
 const token = authStorageService().getToken();
 const user = authStorageService().getUserInfo();
@@ -29,10 +18,10 @@ const initialState = {
 // Register user
 export const register = createAsyncThunk(
   `auth/${authType.REGISTER}`,
-  async (user: RegisterUserInterface, thunkAPI) => {
+  async (user, thunkAPI) => {
     try {
       return await authService.register(user);
-    } catch (error: any) {
+    } catch (error) {
       const message = error?.response?.data?.message;
       return thunkAPI.rejectWithValue(message);
     }
@@ -42,23 +31,10 @@ export const register = createAsyncThunk(
 // Login user
 export const login = createAsyncThunk(
   `auth/${authType.LOGIN}`,
-  async (user: LoginUserInterface, thunkAPI) => {
+  async (user, thunkAPI) => {
     try {
       return await authService.login(user);
-    } catch (error: any) {
-      const message = error?.response?.data?.message;
-      return thunkAPI.rejectWithValue(message);
-    }
-  }
-);
-
-// Get current user
-export const getUserMe = createAsyncThunk(
-  `auth/${authType.GET_USER_ME}`,
-  async (_, thunkAPI) => {
-    try {
-      return await authService.getUserMe();
-    } catch (error: any) {
+    } catch (error) {
       const message = error?.response?.data?.message;
       return thunkAPI.rejectWithValue(message);
     }
@@ -73,23 +49,10 @@ export const logout = createAsyncThunk(`auth/${authType.LOGOUT}`, async () => {
 // Update user
 export const updateUser = createAsyncThunk(
   `auth/${authType.UPDATE_USER}`,
-  async (updatedUserData: any, thunkAPI) => {
+  async (updatedUserData, thunkAPI) => {
     try {
       return await authService.updateUser(updatedUserData);
-    } catch (error: any) {
-      const message = error?.response?.data?.message;
-      return thunkAPI.rejectWithValue(message);
-    }
-  }
-);
-
-// Change password
-export const changePassword = createAsyncThunk(
-  `auth/${authType.CHANGE_PASSWORD}`,
-  async (updatedUserData: any, thunkAPI) => {
-    try {
-      return await authService.changePassword(updatedUserData);
-    } catch (error: any) {
+    } catch (error) {
       const message = error?.response?.data?.message;
       return thunkAPI.rejectWithValue(message);
     }
@@ -117,7 +80,7 @@ export const authSlice = createSlice({
         state.isSuccess = authType.REGISTER;
         state.user = action.payload;
       })
-      .addCase(register.rejected, (state, action: any) => {
+      .addCase(register.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = authType.REGISTER;
         state.message = action.payload;
@@ -131,26 +94,13 @@ export const authSlice = createSlice({
         state.isSuccess = authType.LOGIN;
         state.user = action.payload;
       })
-      .addCase(login.rejected, (state, action: any) => {
+      .addCase(login.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = authType.LOGIN;
         state.message = action.payload;
         state.user = null;
       })
-      .addCase(getUserMe.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(getUserMe.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.isSuccess = authType.GET_USER_ME;
-        state.user = { token, ...action.payload };
-      })
-      .addCase(getUserMe.rejected, (state, action: any) => {
-        state.isLoading = false;
-        state.isError = authType.GET_USER_ME;
-        state.message = action.payload;
-        state.user = null;
-      })
+
       .addCase(logout.fulfilled, (state) => {
         state.user = null;
       })
@@ -162,22 +112,9 @@ export const authSlice = createSlice({
         state.isSuccess = authType.UPDATE_USER;
         state.user = action.payload;
       })
-      .addCase(updateUser.rejected, (state, action: any) => {
+      .addCase(updateUser.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = authType.UPDATE_USER;
-        state.message = action.payload;
-      })
-      .addCase(changePassword.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(changePassword.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.isSuccess = authType.CHANGE_PASSWORD;
-        state.user = action.payload;
-      })
-      .addCase(changePassword.rejected, (state, action: any) => {
-        state.isLoading = false;
-        state.isError = authType.CHANGE_PASSWORD;
         state.message = action.payload;
       });
   },
--- src/stores/index.js
+++ src/stores/index.js
@@ -1,14 +1,14 @@
 /** @format */
 
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth/authSlice";
-import questionReducer from "./questions/questionSlice";
-import userReducer from "./users/userSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth/authSlice';
+// import questionReducer from "./questions/questionSlice";
+// import userReducer from "./users/userSlice";
 
 export const store = configureStore({
-	reducer: {
-		auth: authReducer,
-		questions: questionReducer,
-		users: userReducer,
-	},
+  reducer: {
+    auth: authReducer,
+    // questions: questionReducer,
+    // users: userReducer,
+  },
 });
