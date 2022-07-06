8d15f988cb3d4e6239f4476fde2bac3bb38b18ef
kimhuy011199 <kimhuy011199@gmail.com>
2022-07-06 22:21:26 +0700

    Search by name / ingredient

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,9 +8,7 @@ const Banner = () => {
   const [searchQuery, setSearchQuery] = useState(null);
   const getQuery = (event) => setSearchQuery(event.target.value);
   const [searchBy, setSearchBy] = useState('name');
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  };
+  const getSearchBy = (event) => setSearchBy(event.target.value);
 
   return (
     <section className="banner d-flex align-items-center text-end">
@@ -31,7 +29,7 @@ const Banner = () => {
                 name="type"
                 value="name"
                 defaultChecked="name"
-                onChange={handleChange}
+                onChange={getSearchBy}
               />
               <label htmlFor="name">Name</label>
               <input
@@ -39,12 +37,12 @@ const Banner = () => {
                 id="ingredient"
                 name="type"
                 value="ingredient"
-                onChange={handleChange}
+                onChange={getSearchBy}
               />
               <label htmlFor="ingredient">Ingredient</label>
             </div>
           </div>
-          <Link to={`/search?name=${searchQuery}`}>
+          <Link to={`/search?${searchBy}=${searchQuery}`}>
             <button
               onClick={() => window.scrollBy(0, 525)}
               className="btn btn-warning search-btn rounded-pill"
