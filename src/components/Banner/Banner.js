e6ee652b8bd125564d992ecb4baaeb2bdc77438c
khanhquynh133 <65670847+khanhquynh133@users.noreply.github.com>
2022-07-07 19:43:28 +0700

    update UI

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -11,38 +11,59 @@ const Banner = () => {
 	const getSearchBy = (event) => setSearchBy(event.target.value);
 
 	return (
-		<section className='banner d-flex align-items-center text-end'>
+		// <section className='banner d-flex align-items-center text-center'>
+		// 	<div className='container'>
+		// 		<div className='search-box col-md-5 my-2 d-flex mx-auto'>
+		// 			<input
+		// 				type='text'
+		// 				id='query'
+		// 				onChange={getQuery}
+		// 				className='form-control'
+		// 				placeholder='Find recipe in here ...'
+		// 			/>
+
+		// 			<Link to={`/search?${searchBy}=${searchQuery}`}>
+		// 				<button
+		// 					onClick={() => window.scrollBy(0, 525)}
+		// 					className='btn btn-warning search-btn rounded-pill'>
+		// 					<b>Search</b>
+		// 				</button>
+		// 			</Link>
+		// 		</div>
+		// <input
+		// 	type='radio'
+		// 	id='name'
+		// 	name='type'
+		// 	value='name'
+		// 	defaultChecked='name'
+		// 	onChange={getSearchBy}
+		// />
+		// <label htmlFor='name'>Name</label>
+		// <input
+		// 	type='radio'
+		// 	id='ingredient'
+		// 	name='type'
+		// 	value='ingredient'
+		// 	onChange={getSearchBy}
+		// />
+
+		// 		<h1 className='slogan text-warning'>Choose clean recipes </h1>
+		// 		<h1 className='slogan text-warning'>
+		// 			to make your life more healthier
+		// 		</h1>
+		// 	</div>
+		// </section>
+		<section className='banner d-flex align-items-center text-center'>
 			<div className='container'>
-				<div className='search-box col-md-5 my-2 d-flex mx-auto'>
-					<div>
-						<input
-							type='text'
-							id='query'
-							onChange={getQuery}
-							className='form-control'
-							placeholder='Find recipe in here ...'
-						/>
-						<div>
-							<input
-								type='radio'
-								id='name'
-								name='type'
-								value='name'
-								defaultChecked='name'
-								onChange={getSearchBy}
-							/>
-							<label htmlFor='name'>Name</label>
-							<input
-								type='radio'
-								id='ingredient'
-								name='type'
-								value='ingredient'
-								onChange={getSearchBy}
-							/>
-							<label htmlFor='ingredient'>Ingredient</label>
-						</div>
-					</div>
-					<Link to={`/search?${searchBy}=${searchQuery}`}>
+				<div className='search-box col-md-4 my-2 d-flex mx-auto'>
+					<input
+						type='text'
+						id='query'
+						onChange={getQuery}
+						className='form-control'
+						placeholder='Find recipe in here ...'
+					/>
+					<Link to={`/search?name=${searchQuery}`}>
 						<button
 							onClick={() => window.scrollBy(0, 525)}
 							className='btn btn-warning search-btn rounded-pill'>
@@ -50,6 +71,23 @@ const Banner = () => {
 						</button>
 					</Link>
 				</div>
+				<input
+					type='radio'
+					id='name'
+					name='type'
+					value='name'
+					defaultChecked='name'
+					onChange={getSearchBy}
+				/>
+				<label htmlFor='name'>Name</label>
+				<input
+					type='radio'
+					id='ingredient'
+					name='type'
+					value='ingredient'
+					onChange={getSearchBy}
+				/>
+				<label htmlFor='ingredient'>Ingredient</label>
 
 				<h1 className='slogan text-warning'>Choose clean recipes </h1>
 				<h1 className='slogan text-warning'>
