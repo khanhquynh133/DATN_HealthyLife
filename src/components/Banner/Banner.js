5c2031d2c7c3609b670f82782acdaa5028ff14ad
khanhquynh133 <65670847+khanhquynh133@users.noreply.github.com>
2022-07-07 20:02:28 +0700

    Update UI banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -11,48 +11,6 @@ const Banner = () => {
 	const getSearchBy = (event) => setSearchBy(event.target.value);
 
 	return (
-		// <section className='banner d-flex align-items-center text-center'>
-		// 	<div className='container'>
-		// 		<div className='search-box col-md-5 my-2 d-flex mx-auto'>
-		// 			<input
-		// 				type='text'
-		// 				id='query'
-		// 				onChange={getQuery}
-		// 				className='form-control'
-		// 				placeholder='Find recipe in here ...'
-		// 			/>
-
-		// 			<Link to={`/search?${searchBy}=${searchQuery}`}>
-		// 				<button
-		// 					onClick={() => window.scrollBy(0, 525)}
-		// 					className='btn btn-warning search-btn rounded-pill'>
-		// 					<b>Search</b>
-		// 				</button>
-		// 			</Link>
-		// 		</div>
-		// <input
-		// 	type='radio'
-		// 	id='name'
-		// 	name='type'
-		// 	value='name'
-		// 	defaultChecked='name'
-		// 	onChange={getSearchBy}
-		// />
-		// <label htmlFor='name'>Name</label>
-		// <input
-		// 	type='radio'
-		// 	id='ingredient'
-		// 	name='type'
-		// 	value='ingredient'
-		// 	onChange={getSearchBy}
-		// />
-
-		// 		<h1 className='slogan text-warning'>Choose clean recipes </h1>
-		// 		<h1 className='slogan text-warning'>
-		// 			to make your life more healthier
-		// 		</h1>
-		// 	</div>
-		// </section>
 		<section className='banner d-flex align-items-center text-center'>
 			<div className='container'>
 				<div className='search-box col-md-4 my-2 d-flex mx-auto'>
@@ -66,7 +24,7 @@ const Banner = () => {
 					<Link to={`/search?name=${searchQuery}`}>
 						<button
 							onClick={() => window.scrollBy(0, 525)}
-							className='btn btn-warning search-btn rounded-pill'>
+							className='btn btn-warning search-btn rounded-pill name-style '>
 							<b>Search</b>
 						</button>
 					</Link>
@@ -79,7 +37,9 @@ const Banner = () => {
 					defaultChecked='name'
 					onChange={getSearchBy}
 				/>
-				<label htmlFor='name'>Name</label>
+				<label htmlFor='name' className='textclr name-style me-md-4'>
+					Name
+				</label>
 				<input
 					type='radio'
 					id='ingredient'
@@ -87,7 +47,9 @@ const Banner = () => {
 					value='ingredient'
 					onChange={getSearchBy}
 				/>
-				<label htmlFor='ingredient'>Ingredient</label>
+				<label htmlFor='ingredient' className='textclr name-style me-md-4'>
+					Ingredient
+				</label>
 
 				<h1 className='slogan text-warning'>Choose clean recipes </h1>
 				<h1 className='slogan text-warning'>
